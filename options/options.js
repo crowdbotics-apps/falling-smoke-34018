72698199c3a6dc0478d169b3e40f413d7027444f
Crowdbotics <team@crowdbotics.com>
2022-03-06 16:33:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "falling_smoke_34018",
+  url: "https://falling_smoke_34018.botics.co",
+  api: "https://falling_smoke_34018.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
